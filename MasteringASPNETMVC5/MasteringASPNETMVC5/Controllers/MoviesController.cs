1a5452c925a1df6682c8e49a013120e92e43a4e3
girishkg95 <34678175+girishkg95@users.noreply.github.com>
2020-03-16 12:20:48 +0530

    Initial Project Setup Files

---
+++ MasteringASPNETMVC5/MasteringASPNETMVC5/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MasteringASPNETMVC5.Models;
+
+namespace MasteringASPNETMVC5.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie mv = new Movie() { Id = 1, Name = "Shrek!" };
+            
+            return View(mv);
+        }
+    }
+}
